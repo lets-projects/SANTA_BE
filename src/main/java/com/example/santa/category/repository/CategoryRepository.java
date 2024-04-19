6beb71440b6a031b1bdb82bf91f6e224ec4063b1
민지원 <mjiwon9971@naver.com>
2024-04-19 13:39:28 +0900

    Feat: 카테고리 CRUD

---
+++ src/main/java/com/example/santa/category/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.santa.category.repository;
+
+import com.example.santa.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+
+}
