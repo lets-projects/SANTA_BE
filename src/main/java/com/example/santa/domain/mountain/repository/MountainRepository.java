8fee8c083f07765630d565d379260cf712e7e3cc
ChanHyeok <dhrcksgur1@gmail.com>
2024-04-21 22:45:18 +0900

    UserMountain CR 개발

--- src/main/java/com/example/santa/domain/mountain/repository/MountainRepository.java
+++ src/main/java/com/example/santa/domain/mountain/repository/MountainRepository.java
@@ -1,4 +1,10 @@
 package com.example.santa.domain.mountain.repository;
 
-public interface MountainRepository {
+import com.example.santa.domain.mountain.entity.Mountain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MountainRepository extends JpaRepository<Mountain, Long> {
 }
--- src/main/java/com/example/santa/domain/mountain/service/MountainService.java
+++ src/main/java/com/example/santa/domain/mountain/service/MountainService.java
@@ -1,4 +1,7 @@
 package com.example.santa.domain.mountain.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MountainService {
 }
