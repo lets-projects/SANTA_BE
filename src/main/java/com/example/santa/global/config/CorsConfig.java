ab1edaac0062f7726f6f67204a0ddb7ec0d3aee7
민지원 <mjiwon9971@naver.com>
2024-04-23 21:59:09 +0900

    Feat: corsConfig

---
+++ src/main/java/com/example/santa/global/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.santa.global.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+
+        config.addAllowedOriginPattern("http://localhost" ); // 수정된 부분
+        config.addAllowedOriginPattern("http://localhost:5173" );
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
