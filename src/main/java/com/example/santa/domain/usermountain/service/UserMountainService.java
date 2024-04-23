9f4ba9bddc0c50576861f15c6bc288898b5e3b33
ChanHyeok <dhrcksgur1@gmail.com>
2024-04-23 23:05:20 +0900

    usermountain 수정

--- src/main/java/com/example/santa/domain/usermountain/service/UserMountainService.java
+++ src/main/java/com/example/santa/domain/usermountain/service/UserMountainService.java
@@ -2,6 +2,7 @@ package com.example.santa.domain.usermountain.service;
 
 import com.example.santa.domain.mountain.entity.Mountain;
 import com.example.santa.domain.usermountain.dto.UserMountainResponseDto;
+import com.example.santa.domain.usermountain.dto.UserMountainVerifyRequestDto;
 
 import java.util.Date;
 import java.util.List;
@@ -11,6 +12,8 @@ public interface UserMountainService {
     Mountain test(double latitude, double longitude);
 
     UserMountainResponseDto verifyAndCreateUserMountain(double latitude, double longitude, Date climbDate, String userEmail, Long categoryId);
+
+    UserMountainResponseDto verifyAndCreateUserMountain1(UserMountainVerifyRequestDto userMountainVerifyRequestDto);
 
     List<UserMountainResponseDto> getAllUserMountains();
 
