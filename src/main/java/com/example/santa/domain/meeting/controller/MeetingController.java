e02d977a7d0095412d5e3a4915f440d906af2fa4
민지원 <mjiwon9971@naver.com>
2024-04-24 12:10:02 +0900

    Feat: corsConfig

--- src/main/java/com/example/santa/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/example/santa/domain/meeting/controller/MeetingController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5173/"})
 @RequestMapping("/meetings")
 public class MeetingController {
 
