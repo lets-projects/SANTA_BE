63cb74bd469009fdcd2c61bdb391b8fa8ca0eda5
[Cloud]나정균 <dotseven57@gmail.com>
2024-04-24 19:23:57 +0900

    feat: 이메일 인증용 redis 적용

--- src/main/java/com/example/santa/domain/user/controller/UserController.java
+++ src/main/java/com/example/santa/domain/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.santa.domain.user.controller;
 
+import com.example.santa.domain.mail.dto.EmailCheckDto;
+import com.example.santa.domain.mail.dto.EmailRequestDto;
+import com.example.santa.domain.mail.service.EmailSendService;
 import com.example.santa.domain.user.dto.*;
 import com.example.santa.domain.user.service.UserService;
 import com.example.santa.global.security.jwt.JwtToken;
@@ -8,8 +11,10 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -20,11 +25,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+
 @RequestMapping("/api/users")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
+    private final EmailSendService emailSendService;
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "회원가입")
@@ -36,6 +45,29 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(signup);
     }
 
+    @PostMapping("/send-email")
+    @Operation(summary = "이메일 인증코드 보내기", description = "이메일 인증코드 보내기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "성공", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
+    public String sendMail(@RequestBody @Valid EmailRequestDto emailRequestDto) throws MessagingException, UnsupportedEncodingException {
+        log.info("이메일 인증 이메일: {}", emailRequestDto.getEmail());
+        String code = emailSendService.sendSimpleMessage(emailRequestDto.getEmail());
+        log.info("인증코드: {}", code);
+        return code;
+    }
+
+    @PostMapping("/verify-email")
+    @Operation(summary = "이메일 인증", description = "이메일 인증")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "성공", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
+    public Boolean verifyEmail(@RequestBody @Valid EmailCheckDto emailCheckDto) {
+        Boolean verifyEmail = emailSendService.verifyEmail(emailCheckDto.getAuthNumber(), emailCheckDto.getEmail());
+        return verifyEmail;
+    }
+
+
     @PostMapping("/sign-in")
     @Operation(summary = "로그인", description = "로그인")
     @ApiResponses(value = {
@@ -46,7 +78,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(jwtToken);
     }
 
-    @GetMapping("/checkToken")
+    @GetMapping("/check-token")
     public String checkToken(@AuthenticationPrincipal UserDetails userDetails) {
         return userDetails.getUsername();
     }
@@ -114,6 +146,4 @@ public class UserController {
 //                , passwordChangeRequestDto.getOldPassword(), passwordChangeRequestDto.getNewPassword());
 //        return ResponseEntity.status(HttpStatus.OK).body(changePassword);
 //    }
-
-
 }
