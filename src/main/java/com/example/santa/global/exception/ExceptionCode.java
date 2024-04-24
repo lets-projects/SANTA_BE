830138ac3e15f6275a6863c0c73a9a640f354f8e
민지원 <mjiwon9971@naver.com>
2024-04-24 13:52:54 +0900

    Feat: 같은 날짜에 모임 참여 불가

--- src/main/java/com/example/santa/global/exception/ExceptionCode.java
+++ src/main/java/com/example/santa/global/exception/ExceptionCode.java
@@ -10,7 +10,8 @@ public enum ExceptionCode {
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다."),
     CATEGORY_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다."),
     MEETING_NOT_FOUND(HttpStatus.NOT_FOUND, "모임을 찾을 수 없습니다."),
-    ALREADY_PARTICIPATING(HttpStatus.CONFLICT, "이미 참여중인 모임입니다" );
+    ALREADY_PARTICIPATING(HttpStatus.CONFLICT, "이미 참여중인 모임입니다" ),
+    ALREADY_PARTICIPATING_ON_DATE(HttpStatus.CONFLICT, "참여중인 날짜입니다." );
 
 
     private final HttpStatus status;
