db8e9fa1921cecadab1d1c4aa21a7e5ada83707b
민지원 <mjiwon9971@naver.com>
2024-05-06 13:00:18 +0900

    Fix: api response 수정

--- src/main/java/com/example/santa/domain/user/controller/UserController.java
+++ src/main/java/com/example/santa/domain/user/controller/UserController.java
@@ -123,6 +123,8 @@ public class UserController {
 
     @DeleteMapping
     @Operation(summary = "회원 탈퇴", description = "회원 탈퇴")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = void.class)))})
     public ResponseEntity<?> deleteUser(@AuthenticationPrincipal String email) {
         userService.deleteUser(email);
         return ResponseEntity.ok().build();
@@ -130,6 +132,9 @@ public class UserController {
 
     @DeleteMapping("{userId}")
     @Operation(summary = "회원 삭제(관리자)", description = "회원 삭제(관리자)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = void.class))),
+            @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = void.class)))})
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> deleteUser(@RequestParam(name = "userId") Long id) {
         userService.deleteUserFromAdmin(id);
