542d330b0023eb194323d14abe62d44f3391ba36
ChanHyeok <dhrcksgur1@gmail.com>
2024-05-09 21:01:33 +0900

    주석제거/임포트문 삭제

--- src/main/java/com/example/santa/domain/mountain/service/MountainServiceImpl.java
+++ src/main/java/com/example/santa/domain/mountain/service/MountainServiceImpl.java
@@ -1,34 +1,17 @@
 package com.example.santa.domain.mountain.service;
 
-import com.example.santa.domain.category.entity.Category;
-import com.example.santa.domain.category.repository.CategoryRepository;
-import com.example.santa.domain.challege.entity.Challenge;
-import com.example.santa.domain.challege.repository.ChallengeRepository;
+
 import com.example.santa.domain.mountain.dto.MountainResponseDto;
 import com.example.santa.domain.mountain.entity.Mountain;
 import com.example.santa.domain.mountain.repository.MountainRepository;
-import com.example.santa.domain.user.entity.User;
-import com.example.santa.domain.user.repository.UserRepository;
-import com.example.santa.domain.userchallenge.entity.UserChallenge;
-import com.example.santa.domain.userchallenge.repository.UserChallengeRepository;
-import com.example.santa.domain.userchallenge.service.UserChallengeService;
-import com.example.santa.domain.usermountain.dto.UserMountainResponseDto;
-import com.example.santa.domain.usermountain.entity.UserMountain;
-import com.example.santa.domain.usermountain.repository.UserMountainRepository;
 import com.example.santa.global.exception.ExceptionCode;
 import com.example.santa.global.exception.ServiceLogicException;
 import com.example.santa.global.util.mapsturct.MountainResponseDtoMapper;
-import com.example.santa.global.util.mapsturct.UserMountainResponseDtoMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
