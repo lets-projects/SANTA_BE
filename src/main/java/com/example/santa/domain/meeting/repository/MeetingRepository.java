e63e24db0ba0cd5d604d1eef61ef5d6d2ab22cf3
민지원 <mjiwon9971@naver.com>
2024-05-10 11:42:44 +0900

    Refactor: 사용하지 않는 코드 제거

--- src/main/java/com/example/santa/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/example/santa/domain/meeting/repository/MeetingRepository.java
@@ -12,25 +12,13 @@ import java.util.List;
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     Page<Meeting> findByMeetingTags_Tag_Name(String tagName, Pageable pageable);
 
-    @Query("SELECT m FROM Meeting m JOIN m.meetingTags mt JOIN mt.tag t WHERE t.name = :tagName AND m.id < :lastId")
-    Page<Meeting> findByTagNameAndIdLessThan(@Param("tagName") String tagName, @Param("lastId") Long lastId, Pageable pageable);
-
-
     Page<Meeting> findByCategory_Name(String categoryName, Pageable pageable);
-
-    Page<Meeting> findByCategory_NameAndIdLessThan(String categoryName, Long lastId, Pageable pageable);
 
     @Query("SELECT m FROM Meeting m LEFT JOIN m.participant p GROUP BY m.id ORDER BY COUNT(p) DESC")
     Page<Meeting> findAllByParticipantCount(Pageable pageable);
 
-    @Query("SELECT m FROM Meeting m LEFT JOIN m.participant p GROUP BY m.id HAVING m.id < :lastId ORDER BY COUNT(p) DESC, m.id DESC")
-    Page<Meeting> findAllByParticipantCountAndIdLessThan(@Param("lastId") Long lastId, Pageable pageable);
-
     @Query("SELECT m FROM Meeting m JOIN m.participant p WHERE p.user.id = :userId")
     Page<Meeting> findMeetingsByParticipantUserId(@Param("userId") Long userId, Pageable pageable);
 
-    @Query("SELECT m FROM Meeting m JOIN m.participant p WHERE p.user.id = :userId AND m.id < :lastId ORDER BY m.id DESC")
-    Page<Meeting> findMeetingsByParticipantUserIdAndIdLessThan(@Param("userId") Long userId, @Param("lastId") Long lastId, Pageable pageable);
 
-    Page<Meeting> findByIdLessThanOrderByIdDesc(Long lastId, Pageable pageable);
 }
