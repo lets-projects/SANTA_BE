108e90ea24a163ebbb57151e9b4ad1a482bd42b9
[Cloud]나정균 <dotseven57@gmail.com>
2024-06-05 23:36:12 +0900

    feat: 유저테스트코드 작성

--- src/test/java/com/example/santa/domain/user/service/UserServiceImplTest.java
+++ src/test/java/com/example/santa/domain/user/service/UserServiceImplTest.java
@@ -1,9 +1,7 @@
 package com.example.santa.domain.user.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.santa.domain.category.entity.Category;
 import com.example.santa.domain.category.repository.CategoryRepository;
@@ -14,11 +12,9 @@ import com.example.santa.domain.preferredcategory.repository.PreferredCategoryRe
 import com.example.santa.domain.rank.dto.RankingResponseDto;
 import com.example.santa.domain.rank.entity.Ranking;
 import com.example.santa.domain.rank.repository.RankingRepository;
+import com.example.santa.domain.report.entity.Report;
 import com.example.santa.domain.report.repository.ReportRepository;
-import com.example.santa.domain.user.dto.UserResponseDto;
-import com.example.santa.domain.user.dto.UserSignInRequestDto;
-import com.example.santa.domain.user.dto.UserSignupRequestDto;
-import com.example.santa.domain.user.dto.UserUpdateRequestDto;
+import com.example.santa.domain.user.dto.*;
 import com.example.santa.domain.user.entity.Password;
 import com.example.santa.domain.user.entity.Role;
 import com.example.santa.domain.user.entity.User;
@@ -52,12 +48,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -123,6 +117,7 @@ public class UserServiceImplTest {
         user = new User();
         user.setId(1L);
         user.setEmail("user@email.com");
+        user.setPassword(new Password("Password1!"));
         user.setName("test");
         user.setNickname("testNickName");
         user.setPhoneNumber("01011111111");
@@ -321,54 +316,216 @@ public class UserServiceImplTest {
         verify(userRepository).findByEmail(user.getEmail());
         verify(userResponseDtoMapper).toDto(user);
     }
-/*
+
     @Test
-    void updateUser_WithoutNewImage_UpdatesUserInfoOnly() {
-        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
-        when(userResponseDtoMapper.toDto(user)).thenReturn(userResponseDto);
+    void testFindAllReportUser_WithSearch_FilteredUsers() {
+        // given
+        String search = "test";
+        Pageable pageable = PageRequest.of(0, 10);
+        List<UserReportResponseDto> users = Arrays.asList(
+                new UserReportResponseDto(1L, "test1@example.com", "nickname1", "name1", 2L),
+                new UserReportResponseDto(2L, "test2@example.com", "nickname2", "name2", 3L)
+        );
+        Page<UserReportResponseDto> expectedPage = new PageImpl<>(users, pageable, users.size());
+        when(userRepository.findUsersWithReportCount(search, pageable)).thenReturn(expectedPage);
 
-        UserResponseDto result = userService.updateUser(user.getEmail(), updateRequestDto);
-        System.out.println("nick : " + user.getNickname());
-        System.out.println("result nick : " + result.getNickname());
+        // when
+        Page<UserReportResponseDto> result = userService.findAllReportUser(search, pageable);
 
-        UserUpdateRequestDto requestDto = new UserUpdateRequestDto();
-        requestDto.setNickname("NewNickname");
-        requestDto.setPhoneNumber("NewPhone");
-        requestDto.setName("NewName");
-        requestDto.setImageFile(null);
-        requestDto.setImage(existingImageUrl);
-
-        assertNotNull(result);
-//        assertEquals("NewNickname",result.getNickname());
-        assertEquals(user.getNickname(),result.getNickname());
-        verify(s3ImageService, never()).deleteImageFromS3(anyString());
-        verify(s3ImageService, never()).upload(any());
+        // then
+        assertEquals(expectedPage, result);
+        verify(userRepository, times(1)).findUsersWithReportCount(search, pageable);
     }
 
     @Test
-    void updateUser_WithNewImage_UpdatesUserInfoAndImage() {
+    public void testFindAllReportUser_WithoutSearch_AllUsers() {
+        // given
+        String search = "";
+        Pageable pageable = PageRequest.of(0, 10);
+        List<UserReportResponseDto> users = Arrays.asList(
+                new UserReportResponseDto(1L, "user1@example.com", "nickname1", "name1", 1L),
+                new UserReportResponseDto(2L, "user2@example.com", "nickname2", "name2", 4L)
+        );
+        Page<UserReportResponseDto> expectedPage = new PageImpl<>(users, pageable, users.size());
+        when(userRepository.findUsersWithReportCount(search, pageable)).thenReturn(expectedPage);
+
+        // when
+        Page<UserReportResponseDto> result = userService.findAllReportUser(search, pageable);
+
+        // then
+        assertEquals(expectedPage, result);
+        verify(userRepository, times(1)).findUsersWithReportCount(search, pageable);
+    }
+
+    @Test
+    void testChangePassword_Success() {
         // Given
-        MockMultipartFile imageFile = new MockMultipartFile("image", "test.jpg", "image/jpeg", "image data".getBytes());
-//        UserUpdateRequestDto requestDto = new UserUpdateRequestDto();
-        updateRequestDto.setImage(newImageUrl);
+        String email = user.getEmail();
+        String oldPassword = "Password1!";
+        String newPassword = "newPassword";
 
-        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
-        when(s3ImageService.upload(imageFile)).thenReturn(newImageUrl);
-        when(userResponseDtoMapper.toDto(user)).thenReturn(userResponseDto);
-
-        // Act
-        UserResponseDto result = userService.updateUser(user.getEmail(), updateRequestDto);
-
-        System.out.println("new image: "+newImageUrl);
-        System.out.println("exist image: "+user.getImage());
-        // Assert
-        assertNotNull(result);
-        assertEquals(newImageUrl, user.getImage());
-//        verify(s3ImageService).deleteImageFromS3(existingImageUrl);
-        verify(s3ImageService).upload(imageFile);
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        // When
+        String resultEmail = userService.changePassword(email, oldPassword, newPassword);
+        // Then
+        assertEquals(email, resultEmail);
     }
 
- */
+    @Test
+    void testChangePassword_UserNotFound() {
+        // Given
+        String email = "nonexistent@email.com";
+        String oldPassword = "Password1!";
+        String newPassword = "newPassword";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+        // When & Then
+        assertThrows(
+                ServiceLogicException.class, () -> {
+                    userService.changePassword(email, oldPassword, newPassword);
+                }
+        );
+    }
+
+    @Test
+    void testRestPassword_Success() {
+        // Given
+        String email = user.getEmail();
+        String newPassword = "newPassword";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        // When
+        String resultEmail = userService.resetPassword(email, newPassword);
+
+        // Then
+        assertEquals(email, resultEmail);
+    }
+
+    @Test
+    void testRestPassword_UserNotFound() {
+        // Given
+        String email = "nonexistent@email.com";
+        String newPassword = "newPassword";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+        // When & Then
+        assertThrows(
+                ServiceLogicException.class, () -> {
+                    userService.resetPassword(email, newPassword);
+                }
+        );
+    }
+
+    @Test
+    void testDeleteUser_Success() {
+        // Given
+        String email = user.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(reportRepository.findByReportedParticipant(user)).thenReturn(Collections.emptyList());
+
+        // When
+        userService.deleteUser(email);
+
+        // Then
+        verify(userRepository, times(1)).findByEmail(email);
+        verify(reportRepository, times(1)).findByReportedParticipant(user);
+        verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    void testDeletedUser_UserNotFound() {
+        // Given
+        String email = user.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(ServiceLogicException.class, () -> userService.deleteUser(email));
+        verify(userRepository, times(1)).findByEmail(email);
+    }
+
+    // 이메일로 사용자를 찾았지만 사용자가 신고된 이력이 있을때 예외발생
+    @Test
+    void testDeleted_UserHasReports() {
+        // Given
+        String email = user.getEmail();
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(reportRepository.findByReportedParticipant(user)).thenReturn(List.of(new Report()));
+
+        // When & Then
+        assertThrows(ServiceLogicException.class, () -> userService.deleteUser(email));
+        verify(userRepository, times(1)).findByEmail(email);
+        verify(reportRepository, times(1)).findByReportedParticipant(user);
+    }
+
+    @Test
+    void testDeleteUserFromAdmin_Success() {
+        // Given
+        Long userId = user.getId();
+        // When
+        userService.deleteUserFromAdmin(userId);
+        // Then
+        verify(userRepository, times(1)).deleteById(userId);
+    }
+
+    // 이미지, 사용자 정보 둘다 업데이트된 경우
+    @Test
+    public void testUpdateUser_WithImage_UpdatesUserAndImage() {
+        // Given
+        String email = user.getEmail();
+        MockMultipartFile imageFile = new MockMultipartFile("image", "test.jpg", "image/jpeg", "test image content".getBytes());
+        UserUpdateRequestDto requestDto = new UserUpdateRequestDto();
+        requestDto.setImageFile(imageFile);
+        requestDto.setNickname("updatedNickname");
+        requestDto.setPhoneNumber("01012345678");
+        requestDto.setName("updatedName");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        when(s3ImageService.upload(any(MultipartFile.class))).thenReturn("newImageUrl");
+        when(userResponseDtoMapper.toDto(any(User.class))).thenReturn(userResponseDto);
+
+        // When
+        UserResponseDto result = userService.updateUser(email, requestDto);
+
+        // Then
+        assertNotNull(result);
+        verify(s3ImageService, times(1)).upload(any(MultipartFile.class));
+        verify(userRepository, times(1)).findByEmail(email);
+    }
+
+    // 이미지 x, 사용자 정보만 업데이트된 경우
+    @Test
+    public void testUpdateUser_WithoutImage_UpdatesUserInfo() {
+        // given
+        String email = user.getEmail();
+        UserUpdateRequestDto requestDto = new UserUpdateRequestDto();
+        requestDto.setNickname("updateNickname");
+        requestDto.setPhoneNumber("01012345678");
+        requestDto.setName("updateName");
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        when(userResponseDtoMapper.toDto(any(User.class))).thenReturn(userResponseDto);
+
+        // When
+        UserResponseDto result = userService.updateUser(email, requestDto);
+
+        // Then
+        assertNotNull(result);
+        verify(s3ImageService, never()).upload(any(MultipartFile.class));
+        verify(userRepository, times(1)).findByEmail(email);
+    }
+
+    @Test
+    public void testUpdateUser_UserNotFound() {
+        // Given
+        String email = "nonexistent@example.com";
+        UserUpdateRequestDto requestDto = new UserUpdateRequestDto();
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(ServiceLogicException.class, () -> userService.updateUser(email, requestDto));
+    }
+
 //    @Test
 //    void testFindAllUserMountains() {
 //        // Given
